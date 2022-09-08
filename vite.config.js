eeb5f8c3a6565f621668e62f70d51cd746fcab61
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-09-08 14:06:31 +0200

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  build: {
-    minify: 'esbuild',
-    target: 'esnext',
-  },
   plugins: [createHtmlPlugin({ minify: true })],
 });
