32fb54c06c5b6e860c94d5925ecd4b354dab9598
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-09-11 21:15:34 +0200

    Remove jquery from node_modules

--- src/js/owl-carousel.js
+++ src/js/owl-carousel.js
@@ -1,18 +1,16 @@
-$(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
     },
-  });
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
 });
