e76217695e594de06c8b2d7cc0e0a5ad5dc50888
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-09-14 18:10:11 +0200

    Use owl.carousel and jquery from node_modules

--- src/js/owl-carousel.js
+++ src/js/owl-carousel.js
@@ -1,16 +1,21 @@
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 20,
-  autoplay: true,
-  responsive: {
-    0: {
-      items: 1,
+import $ from '../lib/jquery.js';
+import 'owl.carousel';
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
     },
-    600: {
-      items: 2,
-    },
-    1000: {
-      items: 3,
-    },
-  },
+  });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
-
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   plugins: [createHtmlPlugin({ minify: true })],
+  // optimizeDeps: {
+  //   exclude: ['jquery', 'owl.carousel'],
+  // },
 });
